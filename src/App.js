89f4612a80817d942ddeac09e9e0d79167cd1bc6
xImSergi05 <sergisoler03@gmail.com>
2025-05-16 10:31:13 +0200

    canvi_imatges

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./Logo512.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>picha avall</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://i.pinimg.com/736x/23/02/ee/2302ee861a99f83d58330a6ae3892b17.jpg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Picha aquí i veuràs un fantasma amb 4 cafeteres, 3 propietats, dos
+          puys com dos maons que son verí i una culejada per part de Viruzz
         </a>
       </header>
     </div>
---
+++ src/Sergi.js
@@ -0,0 +1,5 @@
+function Sergi() {
+  return <div className="App">Sergi</div>;
+}
+
+export default Sergi;
